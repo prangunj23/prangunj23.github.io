eec1d8714079b1cf4178ec759fde837eb3ee4738
Toba Ojo <71614914+Toba-O@users.noreply.github.com>
2020-11-10 21:25:08 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-test
+$(document).ready(function() {
+        // Transition effect for navbar 
+        $(window).scroll(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 500) { 
+              $('.navbar').addClass('solid');
+          } else {
+              $('.navbar').removeClass('solid');
+          }
+        });
+});
