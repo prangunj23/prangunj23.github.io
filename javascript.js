eb59bda2d0cfa8e0b13bdbae5c8c3b12d381d08e
Toba Ojo <71614914+Toba-O@users.noreply.github.com>
2020-11-14 08:27:00 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,2 @@
-$(document).ready(function() {
-         
-        $(window).scroll(function() {
-          
-          if($(this).scrollTop() > 500) { 
-              $('.navbar').addClass('solid');
-          } 
-          else {
-              $('.navbar').removeClass('solid');
-          }
-        });
-});
 
 
