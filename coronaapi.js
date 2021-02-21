c035192bc01ecc779598efa3cafb176047f81523
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2021-02-21 18:55:58 -0500

    Update coronaapi.js

--- coronaapi.js
+++ coronaapi.js
@@ -3,7 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
    fetch('https://api.covidtracking.com/v1/us/current.json')
       .then(response => response.json())
       .then(data => {
-          console.log(data);
+          const deaths = data[0];
+          document.querySelector('.coronadeathinfo').innerHTML = deaths.death + " have died from the coronavirus pandemic this year in the United States";
           
        });
               
