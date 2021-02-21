766a139a77744e733d74769ff5969ad491bbdd9c
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2021-02-21 18:24:22 -0500

    Create coronaapi.js

---
+++ coronaapi.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+      
+   fetch('https://api.covidtracking.com/v1/us/current.json')
+      .then(response => response.json())
+      .then(data => {
+          console.log(data);
+       });
+              
+  });
