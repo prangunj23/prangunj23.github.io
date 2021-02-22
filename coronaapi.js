51d53dfb0f67f4a911c99f301bd9e2cbc0c17a9e
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2021-02-21 19:33:36 -0500

    Update coronaapi.js

--- coronaapi.js
+++ coronaapi.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-      
-   fetch('https://api.covidtracking.com/v1/us/current.json')
-      .then(response => response.json())
-      .then(data => {
-         function thousands_separators(num)
+      function thousands_separators(num)
              {
                var num_parts = num.toString().split(".");
                num_parts[0] = num_parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
                return num_parts.join(".");
              }
-          const info = data[0];
+   fetch('https://api.covidtracking.com/v1/us/current.json')
+      .then(response => response.json())
+      .then(data => {
+         
+          const localinfo = data[0];
           document.querySelector('.coronadeathsinfo').innerHTML = thousands_separators(info.death) + " people have died from the coronavirus pandemic this year in the United States";
           document.querySelector('.coronavaccinesinfo').innerHTML = thousands_separators(info.positive) + " people have contracted the coronavirus in the United States";
-         
+          
        });
-              
+    fetch('https://api.covid19api.com/summary')
+      .then(response => response.json())
+      .then(data => {
+           
+          console.log(data);
   });
