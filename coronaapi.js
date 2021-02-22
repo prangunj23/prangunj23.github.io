fb59e35dcc7df5b661fa823bf4c5e5fd6b1ddf22
prangunj23 <65430428+prangunj23@users.noreply.github.com>
2021-02-22 12:13:17 -0500

    Update coronaapi.js

--- coronaapi.js
+++ coronaapi.js
@@ -13,5 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
           document.querySelector('.coronadeathsinfo').innerHTML = thousands_separators(localinfo.death) + " people have died from the coronavirus pandemic this year in the United States";
           document.querySelector('.coronavaccinesinfo').innerHTML = thousands_separators(localinfo.positive) + " people have contracted the coronavirus in the United States";
           
-       });  
+       });
+      
+   fetch("https://covid-19-data.p.rapidapi.com/totals", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "dca0b29f8cmshddb3b0e17a561e2p1ead4bjsnb82d93d300ff",
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+	      }
+      })
+      .then(response => response.json())
+      .then(data => {
+         const globalinfo = data[0];
+         document.querySelector('.globaldeathsinfo').innerHTML = globalinfo.deaths;
+         
+});
 });
